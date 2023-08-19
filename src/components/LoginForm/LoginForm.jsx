994d186a72c834b64030e97f0d216233d50107f9
Abbas Naser <abbas9naser@outlook.it>
2023-08-19 19:04:20 +0300

    Sign Up and Back to Login Implemented

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,19 +1,14 @@
 import { useState } from 'react';
 import * as usersService from '../../utilities/users-service';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 
-export default function LoginForm({ setUser }) {
+export default function LoginForm({ setUser, setShowSignUp }) {
   const [credentials, setCredentials] = useState({
     email: '',
     password: ''
@@ -51,37 +46,10 @@ export default function LoginForm({ setUser }) {
       </Typography>
     );
   }
+
   // <p>&nbsp;{error}</p>
   return (
-    <Grid container component="main" sx={{ height: '100vh' }}>
-    <CssBaseline />
-    <Grid
-      item
-      xs={false}
-      sm={4}
-      md={7}
-      sx={{
-        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: (t) =>
-          t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    />
-    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-      <Box
-        sx={{
-          my: 8,
-          mx: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
+      <>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
@@ -128,16 +96,18 @@ export default function LoginForm({ setUser }) {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link
+                href="#"
+                variant="body2"
+                onClick={() => setShowSignUp(true)}
+              >
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
           </Grid>
           <Copyright sx={{ mt: 5 }} />
         </Box>
-      </Box>
-    </Grid>
-  </Grid>
+      </>
     // <div>
     //   <div className="form-container">
     //     <form autoComplete="off" onSubmit={handleSubmit}>
