14ad82340df678cac8e42ce6fa5419bfe5772b70
Hamad Alhibshi <hamad.alhibshi@gmail.com>
2023-08-19 18:55:47 +0300

    navbar implemented with mui but needs fixing

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -18,9 +18,10 @@ function App() {
           <NavBar user={user} setUser={setUser} />
           <Routes>
             {/* Route components in here */}
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product" element={<NewOrderPage />} />
+            <Route path="/athletes" element={<OrderHistoryPage />} />
+            <Route path="/news" element={<OrderHistoryPage />} />
           </Routes>
         </>
         :
