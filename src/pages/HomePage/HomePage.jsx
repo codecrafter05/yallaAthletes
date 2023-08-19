6e6eac0edc6f6069377b61a2cfa7923f28ecdac2
Hamad Alhibshi <hamad.alhibshi@gmail.com>
2023-08-19 21:54:36 +0300

    design homepage almost done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,78 @@
 import React from "react";
+import Typography from '@mui/material/Typography';
+import bgImage from '../../assets/bg.jpeg'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+
 
 
 export default function HomePage() {
 
   return (
     <>
-    <h1>Welcome to yallaAthletes</h1>
+    <article
+      className='article'
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <Typography variant="h2" className='head'>yallaAthletes</Typography>
+    </article>
+    <Card className="boxContainer">
+      <CardActionArea href='/athletes'>
+        <CardMedia
+          component="img"
+          height="140"
+          image={bgImage}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Athletes
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+
+      <CardActionArea href='/products'>
+        <CardMedia
+          component="img"
+          height="140"
+          image={bgImage}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Products
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+
+      <CardActionArea href='/news' className="newsCard">
+        <CardMedia
+          component="img"
+          height="140"
+          image={bgImage}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            News
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
     </>
   )
 }
