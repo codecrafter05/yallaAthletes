02b33483fcabdf452efc5e924f741d9fa94884fc
Abbas Naser <abbas9naser@outlook.it>
2023-08-21 14:24:13 +0300

    remove unnecessary functions

--- src/utilities/athletes-service.js
+++ src/utilities/athletes-service.js
@@ -4,7 +4,6 @@ export async function create(athleteData) {
   return athleteAPI.create(athleteData);
 }
 
-// fetch data of logged in athlete
 export async function getAthlete() {
   return athleteAPI.getAthlete();
 }
@@ -12,14 +11,3 @@ export async function getAthlete() {
 export async function deleteAthlete() {
   return athleteAPI.deleteAthlete();
 }
-
-// export async function getAthlete() {
-//   try {
-//     const athlete = await athleteAPI.getAthlete();
-//     return athlete;
-//   } catch (error) {
-//     console.error('Error fetching athlete data:', error);
-//     throw new Error(error);
-//   }
-// }
-
